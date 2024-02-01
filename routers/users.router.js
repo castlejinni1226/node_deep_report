4b9d85577ecf9dc7610103050ce9d65a10c8f465
김성진 <jinni1226@naver.com>
2024-02-01 15:49:47 +0900

    로그인 API

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { prisma } from '../models/index.js'
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
 
@@ -11,11 +12,22 @@ router.post('/sign-up', async (req,res,next) => {
         return res.status(409).json({message : "이미 존재하는 이메일입니다."});
     }
 
+    if ( password.length < 6) {
+        return res.status(401).json({message : "비밀번호는 6자리 이상이어야합니다."})
+    }
+
+    if ( password !== checkpassword) {
+        return res.status(401).json({message : "비밀번호가 일치하지 않습니다."})
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedcheckPassword = await bcrypt.hash(checkpassword, 10);
+
     const user = await prisma.Users.create({
         data: {
             email,
-            password,
-            checkpassword,
+            password: hashedPassword,
+            checkpassword : hashedcheckPassword,
             name
         }
     });
